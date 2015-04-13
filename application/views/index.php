97a4d97fb2c4b4d1674e06d532c4cb4fc76a5e71
aboixel <a.boixel@gmail.com>
2015-04-13 21:57:30 +0200

    Signed-off-by: aboixel <a.boixel@gmail.com>

---
+++ application/views/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <?php echo link_tag('assets/css/style.css')?>
+        <title>insane</title>
+    </head>
+	<body>
+	<header>
+	
+			<div id="logo">
+			
+			<?php echo img('assets/images/logo.gif')?>
+			</div>
+			
+	<nav>
+        <ul>
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">Top stories</a></li>
+			<li><a href="#">Events</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</nav>
+	
+	<div id ="barrebleue"></div>
+	
+	</header>
+	&nbsp
+	<div id ="conteneur">
+		
+		<p style="color:#0B2161">10 OCT</p>
+			<div id="imagearticle">
+			<?php echo img('assets/images/extrait1.jpg')?>
+			</div>
+			
+			<div id="article">
+			<h1>Mon titre</h1>
+			<div id="posted">posted by <a href="">Marcus L</a></div>
+			<p>Pellentesque habitant morbi tristique senectus et netus 
+			et malesuada fames ac turpis egestas. Vestibulum tortor quam, 
+			feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero 
+			sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat 
+			eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum 
+			erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, 
+			elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. 
+			Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, 
+			tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, 
+			accumsan porttitor, facilisis luctus, metus</p>
+			</div>
+		
+			<div id="imagearticle">
+				<?php echo img('assets/images/extrait2.jpg')?>
+			</div>
+			
+			<div id="article">
+			<h1>Mon titre</h1>
+			<div id="posted">posted by <a href="">Marcus L</a></div>
+			<p>Pellentesque habitant morbi tristique senectus et netus 
+			et malesuada fames ac turpis egestas. Vestibulum tortor quam, 
+			feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero 
+			sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat 
+			eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum 
+			erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, 
+			elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. 
+			Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, 
+			tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, 
+			accumsan porttitor, facilisis luctus, metus</p>
+			</div>
+	</div>	
+	
+	
+	<div id ="derniersposts">
+		<div>
+		
+		 <?php echo img('assets/images/recentpost1.jpg')?>
+		 <p>Do you see me?</p>
+		</div>
+		<div>
+		 <?php echo img('assets/images/recentpost2.jpg')?>
+		 <p>Usa new leader?</p>
+		</div>
+		
+		<div>
+		 <?php echo img('assets/images/recentpost3.jpg')?>
+
+		</div>
+		&nbsp
+		<div>
+		 <?php echo img('assets/images/recentpost4.jpg')?>
+
+		</div>
+	
+		</div>
+   
+   
+    </body>
+</html>
